2952c7b3db13b2e7e4358ea6766cfc19bf4e5548
hartmetzls <hartmetzls@gmail.com>
2017-06-29 15:02:54 -0400

    removed notes

--- movies/media.py
+++ movies/media.py
@@ -1,17 +1,18 @@
 import webbrowser
 
 
-class Movie(object): #object was a recommendation given by my tutor
+class Movie(object):
     """This class provides a way to store movie-related information."""
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        #you can think of self as itself or the instance in question
+        # you can think of self as itself or the instance in question
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Opens a trailer for the movie from youtube"""
         webbrowser.open(self.trailer_youtube_url)
 
 
